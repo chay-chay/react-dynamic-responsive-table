c3171ec28c64e7b88aba31635b8647d6beff3664
Chayanit <chayanit.chaisri@gmail.com>
2021-09-13 16:21:49 -0400

    Import driverData & tracksData and pass props

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import './App.scss';
+import React, { Fragment } from 'react';
+import Header from './components/Header'
+import Table from './components/Table'
+import driversData from './driversData'
+import tracksData from './tracksData'
 
 const App = () => {
+  // console.log(tracksData)
   return (
-    <div>
-      <h1>React Dynamic & Responsive Table</h1>
-    </div>
+    <Fragment>
+      <Header title='Dynamic &amp; Responsive Table Component' />
+      <Table tableData={driversData} headingColumns={['#', 'Name', 'Team', 'Country', 'Date of birthday', 'Place of birth']} title='F1 Drivers 2020' />
+      <Table 
+      tableData={tracksData} 
+      headingColumns={['Name', 'Country', 'Length(KM)', 'Number of laps']} 
+      title='Top F1 tracks' 
+      breakOn='small' />
+      
+    </Fragment>
   );
 }
 
